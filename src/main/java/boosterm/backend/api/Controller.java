c3a3ff620370221aa5173de54ae7a08944665a1f
Braian Halblaub <braianhal@gmail.com>
2018-08-14 19:01:58 -0300

    Google news api

--- src/main/java/boosterm/backend/api/Controller.java
+++ src/main/java/boosterm/backend/api/Controller.java
@@ -1,14 +1,27 @@
 package boosterm.backend.api;
 
+import boosterm.backend.client.GoogleNewsClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    @Autowired
+    public GoogleNewsClient googleNewsClient;
+
     @GetMapping("/")
     public String index() {
         return "Success!";
+    }
+
+    @GetMapping("/news")
+    public String newsTest(@RequestParam String text) {
+        googleNewsClient.testMethod(text);
+        return "Ok!";
     }
 
 }
---
+++ src/main/java/boosterm/backend/client/GoogleNewsClient.java
@@ -0,0 +1,41 @@
+package boosterm.backend.client;
+
+import com.kwabenaberko.newsapilib.NewsApiClient;
+import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest;
+import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GoogleNewsClient {
+
+    @Value("${api.news.api_key}")
+    private String apiKey;
+
+    private static String DEFAULT_LANGUAGE = "es";
+
+    private NewsApiClient apiClient() {
+        return new NewsApiClient(apiKey);
+    }
+
+    public void testMethod(String text) {
+        apiClient().getTopHeadlines(
+                new TopHeadlinesRequest.Builder()
+                        .q(text)
+                        .language(DEFAULT_LANGUAGE)
+                        .build(),
+                new NewsApiClient.ArticlesResponseCallback() {
+                    @Override
+                    public void onSuccess(ArticleResponse response) {
+                        System.out.println(response.getArticles().get(0).getTitle());
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        System.out.println(throwable.getMessage());
+                    }
+                }
+        );
+    }
+
+}
