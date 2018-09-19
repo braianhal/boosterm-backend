47861797da3bff248b10a867e442366f3a0834b5
felipecalvo <felipecalvo239@gmail.com>
2018-09-18 21:55:40 -0300

    Controller y service para devolver feed de noticias

--- src/main/java/boosterm/backend/api/GraphController.java
+++ src/main/java/boosterm/backend/api/GraphController.java
@@ -1,11 +1,15 @@
 package boosterm.backend.api;
 
+import boosterm.backend.api.response.ArticleResponse;
 import boosterm.backend.api.response.TweetResponse;
 import boosterm.backend.domain.CustomDuration;
-import boosterm.backend.domain.Search;
+import boosterm.backend.domain.NewsSearch;
+import boosterm.backend.domain.TwitterSearch;
 import boosterm.backend.service.GraphService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import twitter4j.TwitterException;
 
 import java.time.LocalDate;
@@ -14,6 +18,8 @@ import java.util.List;
 import java.util.Map;
 
 import static java.util.stream.Collectors.toList;
+
+import java.io.IOException;
 
 @CrossOrigin
 @RestController
@@ -28,7 +34,7 @@ public class GraphController {
                                              @RequestParam String lang,
                                              @RequestParam(name = "limit_amount") int limitAmount,
                                              @RequestParam(name = "limit_type") String limitType) {
-        Search search = new Search(term, lang, new CustomDuration(limitAmount, ChronoUnit.valueOf(limitType)));
+        TwitterSearch search = new TwitterSearch(term, lang, new CustomDuration(limitAmount, ChronoUnit.valueOf(limitType)));
         try {
             return service.getTweetFeed(search).stream().map(TweetResponse::new).collect(toList());
         } catch (TwitterException e) {
@@ -41,12 +47,26 @@ public class GraphController {
                                                                 @RequestParam String lang,
                                                                 @RequestParam(name = "limit_amount") int limitAmount,
                                                                 @RequestParam(name = "limit_type") String limitType) {
-        Search search = new Search(term, lang, new CustomDuration(limitAmount, ChronoUnit.valueOf(limitType)));
+        TwitterSearch search = new TwitterSearch(term, lang, new CustomDuration(limitAmount, ChronoUnit.valueOf(limitType)));
         try {
             return service.getPopularityValueInTimeForTweets(search);
         } catch (TwitterException e) {
             throw new RuntimeException("Can't retrieve data");
         }
     }
-
+    
+	@GetMapping("/feeds/news")
+    public List<ArticleResponse> getNewsFeed(@RequestParam String term,
+    										 @RequestParam String lang) {	
+		try {
+			
+			NewsSearch search = new NewsSearch(term, lang);
+			
+			return service.getNewsFeed(search);
+			
+			
+		} catch (IOException e) {
+			throw new RuntimeException(e.getMessage());
+		}
+    }
 }
--- src/main/java/boosterm/backend/service/GraphService.java
+++ src/main/java/boosterm/backend/service/GraphService.java
@@ -1,12 +1,28 @@
 package boosterm.backend.service;
 
+import boosterm.backend.api.response.ArticleResponse;
 import boosterm.backend.client.TwitterClient;
-import boosterm.backend.domain.Search;
+import boosterm.backend.domain.TwitterSearch;
+import boosterm.backend.domain.NewsApi;
+import boosterm.backend.domain.NewsSearch;
 import boosterm.backend.domain.Tweet;
+
+import org.apache.http.client.fluent.Request;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+
 import twitter4j.TwitterException;
 
+import static java.util.stream.Collectors.toList;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -21,17 +37,20 @@ public class GraphService {
     @Autowired
     public TwitterClient twitter;
 
+    @Value("${api.news.key}")
+    private String newsApiKey;
+    
     private static int TWEET_FEED_COUNT = 10;
 
     private static int TWEET_POPULARIRY_COUNT = 100;
 
-    public List<Tweet> getTweetFeed(Search search) throws TwitterException {
+    public List<Tweet> getTweetFeed(TwitterSearch search) throws TwitterException {
         LocalDateTime now = now();
         LocalDateTime since = search.sinceDate(now);
         return twitter.searchRelevantTweets(search.getTerm(), search.getLanguage(), since, now, TWEET_FEED_COUNT);
     }
 
-    public Map<LocalDate, Integer> getPopularityValueInTimeForTweets(Search search) throws TwitterException {
+    public Map<LocalDate, Integer> getPopularityValueInTimeForTweets(TwitterSearch search) throws TwitterException {
         LocalDate now = now().toLocalDate();
         LocalDate since = search.sinceDate(now);
         HashMap<LocalDate, Integer> popularityMap = new HashMap<>();
@@ -43,6 +62,21 @@ public class GraphService {
             since = since.plusDays(1);
         }
         return popularityMap;
+    }
+    
+    public List<ArticleResponse> getNewsFeed(NewsSearch search) throws IOException {
+    	
+    	String url = "https://newsapi.org/v2/everything?q=" + search.getTerm() + "&language=" + search.getLanguage() + "&sortBy=publishedAt&apiKey=" + newsApiKey;
+		InputStream stream = Request.Get(url)
+				.execute().returnContent().asStream();
+	    	
+		ObjectMapper objectMapper = new ObjectMapper().registerModule(new ParameterNamesModule())
+				   .registerModule(new Jdk8Module())
+				   .registerModule(new JavaTimeModule());
+		
+		NewsApi news = objectMapper.readValue(stream, NewsApi.class);
+		
+		return news.getArticles().stream().map(ArticleResponse::new).collect(toList());
     }
 
 }
